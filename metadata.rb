a14e6a528cd1a4cff018ed1b2e1083f23ee46679
John Esmet <john.esmet@gmail.com>
2014-03-28 21:24:47 +0000

    Add license, some metadata, and a trivial Gemfile

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name                'tokumx'
+maintainer          'esmet'
+maintaner_email     'john.esmet@gmail.com'
+license             'Apache 2.0'
+description         'Wrapper cookbook over github.com/edelight/chef-mongodb for TokuMX'
+version             '0.1.0'
+
+recipe 'tokumx::tokutek_repo', 'Adds the tokutek package repo'
+
+depends 'mongodb', '>= 0.15.1'
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
